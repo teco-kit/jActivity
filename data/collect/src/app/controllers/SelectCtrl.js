359fab64f559b1106cff10fda77b143a0d9d922a
David Greiner <greiner@teco.edu>
2017-01-31 13:33:10 +0100

    Fix 3

--- data/collect/src/app/controllers/SelectCtrl.js
+++ data/collect/src/app/controllers/SelectCtrl.js
@@ -31,10 +31,10 @@ export default function($scope, $filter, $location, ModelService, host, sharedCo
     return ModelService.getLabels().then(function(response) {
 	  const series = response.data.results[0].series;
       //var labels = response.data;
-      var labels = [].concat.apply([], series.map(e=>e.values.filter(x=>x[0]==='label').map(x=>x[1]))).filter((e,i,s)=>s.indexOf(e)===i);
+      var data = [].concat.apply([], series.map(e=>e.values.filter(x=>x[0]==='label').map(x=>x[1]))).filter((e,i,s)=>s.indexOf(e)===i);
       //const labels = [];
       //var labels = series.map(e=>e.values.filter(x=>x[0]==='label').map(x=>x[1])).reduce((a,b)=>a.IndexOf(b)<0?a.concat(b):a);
-      labels = labels.map(e=> ({name:e}));
+      var labels = data.map(e=> ({name:e}));
 	  //series.forEach(e => e.values.forEach(v => { if (v[0] === 'label' && labels.indexOf(v[1]) === -1) labels.push({name: v[1]});}));
       return labels.filter(function(label) {
         return label.name.toLowerCase().indexOf($query.toLowerCase()) != -1;
