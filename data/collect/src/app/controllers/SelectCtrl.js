7092c22e212193e0ed1cb468f16f84e310218e49
David Greiner <greiner@teco.edu>
2017-01-31 12:55:52 +0100

    Fix label selection

--- data/collect/src/app/controllers/SelectCtrl.js
+++ data/collect/src/app/controllers/SelectCtrl.js
@@ -31,9 +31,10 @@ export default function($scope, $filter, $location, ModelService, host, sharedCo
     return ModelService.getLabels().then(function(response) {
 	  const series = response.data.results[0].series;
       //var labels = response.data;
-      const labels = [].concat.apply([], series.map(e=>e.values.filter(x=>x[0]==='label').map(x=>x[1]))).filter((e,i,s)=>s.indexOf(e)===i);
-      //const labels = [];
-	  //response.data.results[0].series.forEach(e => e.values.forEach(v => { if (v[0] === 'label' && labels.indexOf(v[1]) === -1) labels.push(v[1]);}));
+      //const labels = [].concat.apply([], series.map(e=>e.values.filter(x=>x[0]==='label').map(x=>x[1]))).filter((e,i,s)=>s.indexOf(e)===i);
+      var labels = [];
+      //series.map(e=>e.values.filter(x=>x[0]==='label').map(x=>x[1])).reduce((a,b)=>a.IndexOf(b)<0?a.concat(b):a)
+	  series.forEach(e => e.values.forEach(v => { if (v[0] === 'label' && labels.indexOf(v[1]) === -1) labels.push({text: v[1]});}));
       return labels.filter(function(label) {
         return label.toLowerCase().indexOf($query.toLowerCase()) != -1;
       });
